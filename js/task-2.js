3a032b557d5652fba0210f3177fd7be4b73f26ce
za3DopoGo <143600017+za3DopoGo@users.noreply.github.com>
2023-12-03 09:10:54 +0200

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,19 @@
 class Storage {
+  #items;
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    this.items = this.items.filter(item => item !== itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,19 @@
 class StringBuilder {
+  #value;
   constructor(initialValue) {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
 
   getValue() {
-    return this.value;
+    return this.#value;
   }
 
   padEnd(str) {
-    this.value = this.value + str;
+    this.#value += str;
   }
 
   padStart(str) {
-    this.value = str + this.value;
+    this.#value = str + this.#value;
   }
 
   padBoth(str) {
